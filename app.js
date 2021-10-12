94964cb15899adee1ead497048de0a1addebe308
LeanBorto <hlbortoluzzi@gmail.com>
2021-10-11 21:12:53 -0300

    fase 1 sprint 2 v5

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('./public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     const archivo = path.join(__dirname, './views/home.html')
     res.sendFile(archivo);
 });
 
+app.get('/signup', (req, res) => {
+    const archivo = path.join(__dirname, './views/signup.html')
+    res.sendFile(archivo);
+});
+
+app.post('/signup', (req, res) => {
+    req.body;
+    res.redirect('/');
+});
+
+app.get('/signin', (req, res) => {
+    const archivo = path.join(__dirname, './views/signin.html')
+    res.sendFile(archivo);
+});
 app.listen(3000, () => {
     console.log("Servidor corriendo");
 });
