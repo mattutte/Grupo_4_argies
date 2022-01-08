bad4727b0aa4dfe7fb624694cfb2edf6560114a2
Tomas Heguy <tomasheguy@Tomass-MacBook-Air.local>
2022-01-07 23:24:41 -0300

    modifique el controler de home, product serach, produto individiual, y edicion de producto... todo con sequilize, y di algunos ajustes mejores a la estructura de casaka_db.sql... de la parte del controller de producto, queda hacer con sequilize la creacion y eliminacion de productos, y modificar todas las vista con la nueva estructura de sql y los nombre de los campos de los formularios especificados en el controler, mas toda la parte usuarios

--- casaka_db.sql
+++ casaka_db.sql
@@ -44,12 +44,12 @@ CREATE TABLE products (
    rating_value NUMERIC,
    rating_num_comments INT,
    returnable TINYINT(1),
+   weigh_package NUMERIC NOT NULL,
    delivery_time VARCHAR(200),
    color_available VARCHAR(200),
    size_available TEXT,
-   q_available INT,
-   image_full TEXT NOT NULL,
-   image_fron TEXT,
+   image_main TEXT NOT NULL,
+   image_front TEXT,
    image_back TEXT,
    PRIMARY KEY (id),
    FOREIGN KEY (brand) REFERENCES brands (id)
@@ -92,4 +92,4 @@ CREATE TABLE shopping_cart_content (
 /* LOCK TABLES users WRITE;*/
 
 INSERT INTO users VALUES ("tomasheguy@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Tomas","Heguy","Argentina","foto-1639325362742.jpeg",1,1), ("angeluisq96@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Angel","Luis","Venezuela","foto-1639325362742.jpeg",1,1), ("ing.rainero@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Matias","Rainero","Argentina","foto-1639325362742.jpeg",1,1), ("deniijang@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Denis","Jang","Argentina","foto-1639325362742.jpeg",1,1),("hlbortoluzzi@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Leandro","Bortoluzzi","Argentina","foto-1639325362742.jpeg",1,1);
-
+INSERT INTO brands VALUES (DEFAULT,"Adidas","Alemania"), (DEFAULT,"Puma","Alemania"), (DEFAULT,"Nike","Estados Unidos"), (DEFAULT,"Asics","Japon"),(DEFAULT,"Umbro","Reino Unido"),(DEFAULT,"Kappa","Italia"),(DEFAULT,"Hummel","Alemania"),(DEFAULT,"Under Armour","Estados Unidos");
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,7 +5,7 @@ const { validationResult } = require("express-validator")
 
 const productsFilePath = path.join(__dirname, '../data/products.json');
 const usuariosFilePath = path.join(__dirname, '../data/users/users.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); // pasa el json a un array
+//const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8')); // pasa el json a un array
 const usuarios = JSON.parse(fs.readFileSync(usuariosFilePath, 'utf-8')); // pasa el json a un array
 const basketFilePath = path.join(__dirname, '../data/shopping-cart.json');
 const cart_basket = JSON.parse(fs.readFileSync(basketFilePath, 'utf-8'));
@@ -17,19 +17,54 @@ const db = require('../database/models')
 // Acá nos falta un objeto literal con las acciones para cada ruta
 let mainController = {
     home: (req, res) => {
-        console.log('entré al home');
-        res.render('home', { products })
+        db.product.findAll({
+            order:[['rating','DESC']],
+            include:[{association:'brand'}]
+    }).then((products)=>{
+        res.render('home',{products,brands})
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.send(500);
+    });
+
+        // modificar con sequilize
+        //console.log('entré al home');
+        //res.render('home', { products })
     },
 
     product: (req, res) => {
-        const product = products.find((prod) => prod.id == req.params.id);
-
-        res.render('product', { products, product })
+        const id = req.params.id;
+        db.product.findByPk(id)({
+            include:[{association:'brand'}]
+        })
+        .then((product)=>{
+            res.render('product', {product,brand});
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.send(500);
+        });
+        
+        // modificar con sequilize
+        //const product = products.find((prod) => prod.id == req.params.id);
+        //res.render('product', { products, product })
     },
 
     productSearch: (req, res) => {
-        //productos = 
-        res.render('productSearch', { products, resultsPerPage: 12 })
+
+        db.product.findAll({
+            order:[['rating','DESC']],
+            include:[{association:'brand'}]
+        }).then((products)=>{
+            res.render("productSearch", { products, resultsPerPage: 12 })
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.send(500);
+        });
+        //modificar con sequilize 
+        //res.render('productSearch', { products, resultsPerPage: 12 })
     },
 
     signin: (req, res) => {
@@ -136,10 +171,12 @@ let mainController = {
     },
 
     addProduct: (req, res) => {
+        // modificar form segun nuevo SQL
         res.render('product-add-form v2')
     },
 
     store: (req, res) => {
+        // modificar con sequilize y nueva estructure SQL
         console.log(req.body);
         console.log(req.files);
 
@@ -168,7 +205,7 @@ let mainController = {
         })
 
         const nuevoProducto = {
-
+            // modificar con sequilize y nueva estructure SQL
             id: products[products.length - 1].id + 1, // le crea un id 1 mas alto que el del ultimo
             name: req.body.name ? req.body.name : "",
             brand: req.body.brand ? req.body.brand : "",
@@ -215,100 +252,162 @@ let mainController = {
     },
 
     pre_edit: (req, res) => {
-        const productToEdit = products.find((prod) => prod.id == req.params.id);
-
-        res.render('product-pre-edit', { productToEdit })
+        const id = req.params.id;
+        db.product.findByPk(id)
+        .then((productToEdit)=>{
+            res.render('product-pre-edit', {productToEdit});
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.send(500);
+        });
+        
+        // modificar form con nueva estructure SQL
+        //const productToEdit = products.find((prod) => prod.id == req.params.id);
+        //res.render('product-pre-edit', { productToEdit })
     },
 
     editProduct: (req, res) => {
-        const productToEdit = products.find((prod) => prod.id == req.params.id);
-        res.render('product-edit-form v2', { productToEdit })
+        const id = req.params.id;
+        db.product.findByPk(id)
+        .then((productToEdit)=>{
+            res.render('product-edit-form v3', {productToEdit});
+        })
+        .catch((error)=>{
+            console.log(error);
+            res.send(500);
+        });
+        
+        // modificar form con nueva estructure SQL
+        //const productToEdit = products.find((prod) => prod.id == req.params.id);
+        //res.render('product-edit-form v2', { productToEdit })
     },
 
     update: (req, res) => {
-        const productIndex = products.findIndex((producto) => {
-            return (producto.id == req.params.id)
+    const id = req.params.id;
+            db.product.update(
+                {
+                    category: req.body.category ? req.body.category : productToEdit.category,
+                    name_product: req.body.name ? req.body.name : productToEdit.name_product,
+                    brand: req.body.brand ? req.body.brand : productToEdit.brand,
+                    description_product: req.body.description ? req.body.description : productToEdit.description_product,
+                    year_created: req.body.year ? req.body.year : productToEdit.year_created,
+                    features_style: req.body.features_style ? req.body.features_style : productToEdit.features_style,
+                    features_gender: req.body.features_style ? req.body.features_gender : productToEdit.features_style,
+                    features_use: req.body.features_style ? req.body.features_use : productToEdit.features_style,
+                    features_others: req.body.features_style ? req.body.features_others : productToEdit.features_style,
+                    regular_price: Number(req.body.regularPrice) ? req.body.regularPrice : productToEdit.regular_price,
+                    special_price: Number(req.body.specialPrice) ? req.body.specialPrice : productToEdit.special_price,
+                    caption: req.body.caption ? req.body.caption : productToEdit.caption,
+                    returnable: req.body.devolucion == 1? 1 : 0,
+                    delivery_time: req.body.delivery_time ? req.body.delivery_time : productToEdit.delivery_time,
+                    weight_package: req.body.weight_package ? req.body.weight_package : productToEdit.weight_package,
+                    color_available: req.body.color_available ? req.body.color_available : productToEdit.color_available,
+                    size_available: req.body.size_available ? req.body.size_available : productToEdit.size_available,
+                    image_main: req.file["images-main"] ? req.file["images-main"].filename : productToEdit.images_main,
+                    image_front: req.file["images-front"] ? req.file["images-front"].filename : productToEdit.images_front,
+                    image_back: req.file["images-back"] ? req.file["images-back"].filename : productToEdit.images_back,
+                 
+                } ,
+                {where:{id :id}}
+             )
+            
+             .then(function(){  
+                res.redirect('/')
+            })
+            .catch((error)=>{
+                console.log(error);
+                res.send(500);
+            });
 
-        });
+        
+        // modificar con sequilize y nueva estructure SQL
+
+        // const productIndex = products.findIndex((producto) => {
+        //     return (producto.id == req.params.id)
+
+        // });
 
         //console.log(req.files);
         //console.log(req.body);
-        const productToEdit = products.find((prod) => prod.id == req.params.id);
+        // const productToEdit = products.find((prod) => prod.id == req.params.id);
 
         //Procesando características -----------------------------------
-        if (productToEdit.caracteristicas == "") {
-            productToEdit.caracteristicas = ["", "", "", ""];
-        }
+        // if (productToEdit.caracteristicas == "") {
+        //     productToEdit.caracteristicas = ["", "", "", ""];
+        // }
 
-        let caracteristicas_original = [req.body.caract_1 ? req.body.caract_1 : productToEdit.caracteristicas[0], req.body.caract_2 ? req.body.caract_2 : productToEdit.caracteristicas[1], req.body.caract_3 ? req.body.caract_3 : productToEdit.caracteristicas[2], req.body.caract_4 ? req.body.caract_4 : productToEdit.caracteristicas[3]];
-        let caracteristicas_def = []
-            //console.log(caracteristicas_original);
-        let i = 0;
-        for (let carac in caracteristicas_original) {
-            if (caracteristicas_original[carac] != "") {
-                caracteristicas_def.push(caracteristicas_original[carac]);
-                i++;
-            }
-        }
-        console.log(caracteristicas_def);
-        if (i = 0) {
-            caracteristicas_def = "";
-        }
+        // let caracteristicas_original = [req.body.caract_1 ? req.body.caract_1 : productToEdit.caracteristicas[0], req.body.caract_2 ? req.body.caract_2 : productToEdit.caracteristicas[1], req.body.caract_3 ? req.body.caract_3 : productToEdit.caracteristicas[2], req.body.caract_4 ? req.body.caract_4 : productToEdit.caracteristicas[3]];
+        // let caracteristicas_def = []
+        //     //console.log(caracteristicas_original);
+        // let i = 0;
+        // for (let carac in caracteristicas_original) {
+        //     if (caracteristicas_original[carac] != "") {
+        //         caracteristicas_def.push(caracteristicas_original[carac]);
+        //         i++;
+        //     }
+        // }
+        // console.log(caracteristicas_def);
+        // if (i = 0) {
+        //     caracteristicas_def = "";
+        // }
 
         //Procesando talles --------------------------------------------
-        let talles_req = [
-            typeof(req.body.small) != "undefined" ? "Small" : "",
-            typeof(req.body.medium) != "undefined" ? "Medium" : "",
-            typeof(req.body.large) != "undefined" ? "Large" : "",
-            typeof(req.body.extralarge) != "undefined" ? "Extra-Large" : ""
-        ];
-        let talles_def = [];
-        talles_req.forEach(talle => {
-            if (talle !== '') {
-                talles_def.push(talle);
-            }
-        })
+        // let talles_req = [
+        //     typeof(req.body.small) != "undefined" ? "Small" : "",
+        //     typeof(req.body.medium) != "undefined" ? "Medium" : "",
+        //     typeof(req.body.large) != "undefined" ? "Large" : "",
+        //     typeof(req.body.extralarge) != "undefined" ? "Extra-Large" : ""
+        // ];
+        // let talles_def = [];
+        // talles_req.forEach(talle => {
+        //     if (talle !== '') {
+        //         talles_def.push(talle);
+        //     }
+        // })
 
-        const productoEditado = {
-            id: products[productIndex].id,
-            name: req.body.name ? req.body.name : productToEdit.name,
-            brand: req.body.brand ? req.body.brand : productToEdit.brand,
-            description: req.body.description ? req.body.description : productToEdit.description,
-            caracteristicas: caracteristicas_def,
-            detalle: req.body.detalle ? req.body.detalle : productToEdit.detalle,
-            talles: talles_def,
-            regularPrice: Number(req.body.regularPrice) ? "$ " + req.body.regularPrice : productToEdit.regularPrice,
-            specialPrice: Number(req.body.specialPrice) ? "$ " + req.body.specialPrice : productToEdit.specialPrice,
-            cuotas: {
-                banco: req.body.cuotasbanco ? req.body.cuotasbanco : productToEdit.cuotas.banco,
-                cantidad: req.body.cuotas ? req.body.cuotas : productToEdit.cuotas.cantidad
-            },
-            caption: req.body.caption ? req.body.caption : productToEdit.caption,
-            inventario: {
-                disponibilidad: req.body.disponibilidad ? req.body.disponibilidad : productToEdit.inventario.disponibilidad,
-                peso_paq: req.body.peso_paq ? req.body.peso_paq : productToEdit.inventario.peso_paq,
-                devolucion: req.body.devolucion ? req.body.devolucion : productToEdit.inventario.devolucion,
-                tiempoEntrega: req.body.tiempoEntrega ? req.body.tiempoEntrega : productToEdit.inventario.tiempoEntrega
-            },
-            rating: {
-                value: req.body.rating ? Number(req.body.rating) : productToEdit.rating.value,
-                quantity: req.body.quantity ? req.body.quantity : productToEdit.rating.quantity
-            }, // le agrega todo lo del formulario excepto el file
-            images: {
-                main: req.files["images-main"] ? req.files["images-main"][0].filename : productToEdit.images.main,
-                front: req.files["images-front"] ? req.files["images-front"][0].filename : productToEdit.images.front,
-                back: req.files["images-back"] ? req.files["images-back"][0].filename : productToEdit.images.back
-            } //le agrega los files que uploade, si lo hice, sino mantengo el anterior
-        };
+        // const productoEditado = {
+        //     id: products[productIndex].id,
+        //     category: req.body.category ? req.body.category : productToEdit.category,
+        //     name_product: req.body.name ? req.body.name : productToEdit.name_product,
+        //     brand: req.body.brand ? req.body.brand : productToEdit.brand,
+        //     description_product: req.body.description ? req.body.description : productToEdit.description_product,
+        //     year_created: req.body.year ? req.body.year : productToEdit.year_created,
+        //     caracteristicas: caracteristicas_def,
+        //     detalle: req.body.detalle ? req.body.detalle : productToEdit.detalle,
+        //     talles: talles_def,
+        //     regularPrice: Number(req.body.regularPrice) ? "$ " + req.body.regularPrice : productToEdit.regularPrice,
+        //     specialPrice: Number(req.body.specialPrice) ? "$ " + req.body.specialPrice : productToEdit.specialPrice,
+        //     cuotas: {
+        //         banco: req.body.cuotasbanco ? req.body.cuotasbanco : productToEdit.cuotas.banco,
+        //         cantidad: req.body.cuotas ? req.body.cuotas : productToEdit.cuotas.cantidad
+        //     },
+        //     caption: req.body.caption ? req.body.caption : productToEdit.caption,
+        //     inventario: {
+        //         disponibilidad: req.body.disponibilidad ? req.body.disponibilidad : productToEdit.inventario.disponibilidad,
+        //         peso_paq: req.body.peso_paq ? req.body.peso_paq : productToEdit.inventario.peso_paq,
+        //         devolucion: req.body.devolucion ? req.body.devolucion : productToEdit.inventario.devolucion,
+        //         tiempoEntrega: req.body.tiempoEntrega ? req.body.tiempoEntrega : productToEdit.inventario.tiempoEntrega
+        //     },
+        //     rating: {
+        //         value: req.body.rating ? Number(req.body.rating) : productToEdit.rating.value,
+        //         quantity: req.body.quantity ? req.body.quantity : productToEdit.rating.quantity
+        //     }, // le agrega todo lo del formulario excepto el file
+        //     images: {
+        //         main: req.files["images-main"] ? req.files["images-main"][0].filename : productToEdit.images.main,
+        //         front: req.files["images-front"] ? req.files["images-front"][0].filename : productToEdit.images.front,
+        //         back: req.files["images-back"] ? req.files["images-back"][0].filename : productToEdit.images.back
+        //     } //le agrega los files que uploade, si lo hice, sino mantengo el anterior
+        // };
 
         //console.log(productoEditado);
 
-        products[productIndex] = productoEditado;
+        // products[productIndex] = productoEditado;
 
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
+        // fs.writeFileSync(productsFilePath, JSON.stringify(products, null, ' '));
 
 
-        res.redirect('/');
+        // res.redirect('/');
 
 
 
@@ -317,6 +416,7 @@ let mainController = {
 
     // Delete - Delete one product from DB
     destroy: (req, res) => {
+        // modificar con sequilize 
         console.log("llegamos al destroy");
 
 
