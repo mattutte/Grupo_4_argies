013d93af08666a56ebee61d221514b2575e8add2
Tomas Heguy <tomasheguy@Tomass-MacBook-Air.local>
2022-01-10 21:46:40 -0300

    complete controllers de producto,  update del sql, esj de productos, pre_edit y edit .... estos ultimos todavia no completos

--- casaka_db.sql
+++ casaka_db.sql
@@ -89,8 +89,3 @@ CREATE TABLE shopping_cart_content (
 ) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-/* LOCK TABLES users WRITE;*/
-
-INSERT INTO users VALUES ("tomasheguy@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Tomas","Heguy","Argentina","foto-1639325362742.jpeg",1,1), ("angeluisq96@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Angel","Luis","Venezuela","foto-1639325362742.jpeg",1,1), ("ing.rainero@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Matias","Rainero","Argentina","foto-1639325362742.jpeg",1,1), ("deniijang@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Denis","Jang","Argentina","foto-1639325362742.jpeg",1,1),("hlbortoluzzi@gmail.com","$2a$10$P4Y.rnn8NQ9YXbz6DoxEN.SywATZpuSgf2XBx206Oo21SOSrYM8lu","Leandro","Bortoluzzi","Argentina","foto-1639325362742.jpeg",1,1);
-INSERT INTO brands VALUES (DEFAULT,"Adidas","Alemania"), (DEFAULT,"Puma","Alemania"), (DEFAULT,"Nike","Estados Unidos"), (DEFAULT,"Asics","Japon"),(DEFAULT,"Umbro","Reino Unido"),(DEFAULT,"Kappa","Italia"),(DEFAULT,"Hummel","Alemania"),(DEFAULT,"Under Armour","Estados Unidos");
-INSERT INTO products VALUES (DEFAULT,"Jersey","JUVENTUS 21/22 HOME JERSEY BY ADIDAS",44,"",2018,"Short-sleeve Jersey","Hombre","Entrenamiento","",8401,5461,NULL,NULL,1,0.850,"24hs","Rojo","L","camiseta1.jpeg","","");
