75f08d2e1df183992982d84d82d1a872f7e2f9a5
tomasheguy <tomasheguy@gmail.com>
2022-01-26 08:35:54 -0300

    validacion de back end de signup funcionando ... todavia con algunos problemas en la validacion de front end

--- middleware/signupValidationMiddleware.js
+++ middleware/signupValidationMiddleware.js
@@ -5,20 +5,18 @@ const validations = [
     check('email').notEmpty().withMessage('Debe completar un email valido. ').bail()
                     .isEmail().withMessage('Debe completar un email valido. ').bail()
                     
-                    // .custom((value) => {
-                    //     const db = require('../database/models');
-                    //     return db.User.findByPk(value)
-                    //    .then(user => {
-                    //      console.log(user);
-                    //      if (user == null) {
-                    //       return true;
-                    //      }else{
-                    //        return false;
-                    //      }
-                    //    });
-                    //  })
-                    //  .withMessage('Debe completar con un email unico. ').bail()
-                    ,
+                    .custom((value) => {
+                         const db = require('../database/models');
+                         let existingUser = db.User.findByPk(value);
+                         //console.log(existingUser)
+                         return existingUser
+                        .then(user => {
+                          console.log(user);
+                          if (user != null) {
+                            return Promise.reject("El email ya esta en uso");
+                          }
+                        });
+                      }),
 
     check('psw').notEmpty().withMessage('Debe completar una clave de al menos 8 cifras. ').bail()
                 .isLength(8).withMessage('Debe completar una clave de al menos 8 cifras. ').bail(),
@@ -39,7 +37,7 @@ const validations = [
                               console.log('no hay archivo subido');
                               return false;
                             }
-                            if(req.file.mimetype === 'image/jpg' || req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png' || req.file.mimetype === 'image/gif'  ){
+                            else if(req.file.mimetype === 'image/jpg' || req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png' || req.file.mimetype === 'image/gif'  ){
                                 return true; // return "non-falsy" value to indicate valid data"
                             }else{
                                 console.log('el formato del archivo no es el indicado');
--- public/js/signupvalidations.js
+++ public/js/signupvalidations.js
@@ -1,15 +1,18 @@
 window.addEventListener("load", function() {
 
     let email = document.querySelector('[name="email"]');
-    let password = document.querySelector('[name="psw"]');
-    let passwordRepeat = document.querySelector('[name="pswrepeat"]');
+    console.log(email);
+    let psw = document.querySelector('[name="psw"]');
+    console.log(psw);
+    let pswrepeat = document.querySelector('[name="pswrepeat"]');
+    console.log(pswrepeat);
     let first_name = document.querySelector('[name="first_name"]');
     let last_name = document.querySelector('[name="last_name"]');
     let country = document.querySelector('[name="pais"]');
     let face_pic = document.getElementById('facepic');
     
     
-    let formulario = document.querySelector("form");
+    let form = document.querySelector("form");
     let submitButton = document.querySelector(".signupbtn");
 
     
@@ -20,26 +23,42 @@ window.addEventListener("load", function() {
     }
     
 
-    //Validaciones para Email
-    email.addEventListener('change', function(){
-        console.log(emailIsValid(email.value));
+    //Validaciones para Email vieja y que no funcionaba bien
+    // email.addEventListener('change', function(){
+    //     console.log(emailIsValid(email.value));
+    //     if(email.value == '' || !emailIsValid(email.value)){
+    //         //email.classList.add('email-invalido');
+    //         this.style.border = " 2px solid red";
+    //         console.log('el mail esta mal escrito')
+    //         document.querySelector('#comentario-email').innerText = "Email invalido o vacio. ";
+    //     }else{
+    //         //email.classList.remove('email-invalido');
+    //         console.log('entre en el esle');
+    //         this.style.border = " 2px solid blue";
+    //         document.querySelector('#comentario-email').innerText = "";
+    //     }
+    // })
+
+    email.addEventListener('blur', function(){
+        console.log(email.value);
         if(email.value == '' || !emailIsValid(email.value)){
-            //email.classList.add('email-invalido');
+            console.log('hay problemas con el emaiil');
             this.style.border = " 2px solid red";
-            console.log('el mail esta mal escrito')
             document.querySelector('#comentario-email').innerText = "Email invalido o vacio. ";
         }else{
-            //email.classList.remove('email-invalido');
-            console.log('entre en el esle');
+            console.log('no hay problemas con el emaiil');
             this.style.border = " 2px solid blue";
             document.querySelector('#comentario-email').innerText = "";
-        }
+        } 
     })
+    
+
 
 
     //Validaciones para password
-    password.addEventListener('blur', function(){
-        if(password.value == '' || password.value.length < 8){
+    psw.addEventListener('blur', function(){
+        console.log(psw.value);
+        if(psw.value == '' || psw.value.length < 8){
                 //password.classList.add('psw-invalido');
                 this.style.border = " 2px solid red";
                 document.querySelector('#comentario-psw').innerText = "Debe completar una password de al menos 8 caracteres. ";
@@ -53,8 +72,9 @@ window.addEventListener("load", function() {
 
 
     //Validaciones para Repeat Password
-    passwordRepeat.addEventListener('blur', function(){
-        if(passwordRepeat.value === '' || passwordRepeat.value != password.value){
+    pswrepeat.addEventListener('blur', function(){
+        console.log(pswrepeat.value);
+        if(pswrepeat.value === '' || pswrepeat.value != psw.value){
             //passwordRepeat.classList.add('pswrepeat-invalido');
             this.style.border = " 2px solid red";
             document.querySelector('#comentario-pswrepeat').innerText = "Debe repetir la clave y que coincidan. ";
@@ -69,6 +89,7 @@ window.addEventListener("load", function() {
 
     //Validaciones para Nombre
     first_name.addEventListener('blur', function(){
+        console.log(first_name.value);
         if(first_name.value == '' || first_name.value.length < 2){
             //first_name.classList.add('first_name-invalido');
             this.style.border = " 2px solid red";
@@ -84,6 +105,7 @@ window.addEventListener("load", function() {
 
     //Validaciones para Apellido
     last_name.addEventListener('blur', function(){
+        console.log(last_name.value);
         if(last_name.value == '' || last_name.value.length < 2){
             //last_name.classList.add('last_name-invalido');
             this.style.border = " 2px solid red";
@@ -99,6 +121,7 @@ window.addEventListener("load", function() {
 
     //Validaciones para Pais
     country.addEventListener('blur', function(){
+        console.log(country.value);
         if(country.value == '' || country.value.length < 2){
             //last_name.classList.add('country-invalido');
             this.style.border = " 2px solid red";
@@ -113,7 +136,8 @@ window.addEventListener("load", function() {
     face_pic.addEventListener('blurr', function(){
   
         
-        console.lot('facepic');
+        console.log(face_pic);
+        console.log(face_pic.type);
         if(face_pic.name == '' || !(face_pic.type == "jpg" || face_pic.type == "jpeg" || face_pic.type == "gif" || face_pic.type == "png" ) ){
             //last_name.classList.add('face_pic-invalido');
             this.style.border = " 2px solid red";
@@ -132,40 +156,26 @@ window.addEventListener("load", function() {
     //Validacion del boton Submit
     submitButton.addEventListener('click', function(e){
 
-        console.group('entrando al fetch');
-        console.log(email.value);
-
-        //.then(function(informacion){
-            if(informacion!=null){
-                
-                console.log('el email no estaba registrado con anterioridad');
-            } else {
-                e.preventDefault();    
-                alert('El email no puede estar repetido');
-               
-            }
-
+        
+             console.group('entrando al fetch');
+             console.log(email.value);
             
-
-            //  console.group('entrando al fetch');
-            //  console.log(email.value);
-            
-            //  fetch("http://localhost:3000/users/" + email.value)
-            //      .then(function(respuesta){
-            //          console.log('entro al primer then');
-            //          return respuesta.json();
+             fetch("http://localhost:3000/users/" + email.value)
+                 .then(function(respuesta){
+                     console.log('entro al primer then');
+                     return respuesta.json();
                                         
-            //      })
-            //      .then(function(informacion){
-            //          if(informacion!=null){
+                 })
+                 .then(function(informacion){
+                     if(informacion!=null){
                          
-            //              console.log('el email no estaba registrado con anterioridad');
-            //          } else {
-            //              e.preventDefault();    
-            //              alert('El email no puede estar repetido');
+                         console.log('el email no estaba registrado con anterioridad');
+                     } else {
+                         e.preventDefault();    
+                         alert('El email no puede estar repetido');
                         
-            //          }
-            //      });
+                     }
+                 });
 
 
             
